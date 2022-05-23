97cb97321bb91aad66662523cd110a94d5972ce1
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-05-23 20:31:01 +0600

    dinamic id fetch

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
 
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvpcq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,6 +26,14 @@ async function run() {
             const cursor = toolCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get("/tool/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectID(id) }
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
         });
 
     }
