7eb07d671c7cd8eb2072a39bc4a6adc26888befd
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-05-23 16:28:12 +0600

    server set up & data load

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvpcq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('tools_provita').collection('tools');
+
+        // all data load
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Manufacturer Server On !')
+})
+
+app.listen(port, () => {
+    console.log(`Manufacturer app listening on port ${port}`)
+})
